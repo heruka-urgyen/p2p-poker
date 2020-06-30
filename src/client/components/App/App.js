204acbab404836b8205960e3ad3977bdaeb1825a
urgyen <imalyanov@gmail.com>
2020-06-30 14:31:20 -0500

    add table component
    with a sketch of the table

--- src/client/components/App/App.js
+++ src/client/components/App/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import Table from 'client/components/Table'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header" data-testid="App-header">
       </header>
+      <main>
+        <Table />
+      </main>
     </div>
   )
 }
---
+++ src/client/components/Table/Table.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Table() {
+  return (
+    <div className="Table">
+    </div>
+  )
+}
+
+export default Table
