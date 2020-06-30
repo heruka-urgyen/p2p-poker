8d8c1f9fbbf64f3da98ede151494341456c0d1e2
urgyen <imalyanov@gmail.com>
2020-06-30 16:06:28 -0500

    add basic boilerplate for server
    state update logic with immer

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,20 @@
 const io = require('socket.io')()
+const immer = require('immer')
+const {produce} = immer
 
-io.on('connection', socket => {console.log('connected to ' + socket.id)})
+/******************** state management ********************/
+// _state is a private variable not to be directly accessed or mutated
+let _state = {}
+const update = f => _state = produce(_state, d => {f(d)})
+const select = f => produce(_state, f)
+
+/********************* default state *********************/
+
+/******************** socket handlers ********************/
+
+io.on('connection', socket => {
+  console.log('connected to ' + socket.id)
+})
+
 
 io.listen(3001)
