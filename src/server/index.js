36bbb94f1037ae119af4ecd7f531a369d04ae9e6
urgyen <imalyanov@gmail.com>
2020-06-30 17:32:12 -0500

    add get user handler on the server

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 const io = require('socket.io')()
+const cookie = require('cookie')
 const immer = require('immer')
 const {produce} = immer
 
 /******************** state management ********************/
+
 // _state is a private variable not to be directly accessed or mutated
 let _state = {}
 const update = f => _state = produce(_state, d => {f(d)})
@@ -10,10 +12,21 @@ const select = f => produce(_state, f)
 
 /********************* default state *********************/
 
+const defaultUser = {type: 'guest'}
+const users = update(s => {s.users = {}})
+
 /******************** socket handlers ********************/
 
 io.on('connection', socket => {
   console.log('connected to ' + socket.id)
+
+  const c = cookie.parse(socket.request.headers.cookie)['connect.sid']
+  socket.on('GET_USER', _ => {
+    console.log('received GET_USER from', socket.id)
+    const user = select(s => s.users)[c] || defaultUser
+
+    socket.emit('GET_USER_SUCCESS', {payload: {user}})
+  })
 })
 
 
