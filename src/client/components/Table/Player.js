dd4afb6b9c3a4bcdeab32b4b64da62d73357d738
urgyen <imalyanov@gmail.com>
2020-07-01 14:31:02 -0500

    display chips and bet amounts

--- src/client/components/Table/Player.js
+++ src/client/components/Table/Player.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-import {Maybe} from 'client/util'
+import {Maybe, safe} from 'client/util'
+
+import chip from 'client/images/poker-chip.svg'
 
 function Player({i, player, round}) {
-  const buttonPlayerId = round.players[round.button]
+  const buttonPlayerId = safe(null)(() => round.players[round.button])
+  const bet = safe({})(() => round.bets.filter(b => b.playerId === player.id)[0])
 
   return (
     <li className={`player player__${i}`}>
@@ -11,6 +14,16 @@ function Player({i, player, round}) {
       <label className="player-stack">${player.stack}</label>
       <Maybe cond={() => buttonPlayerId === player.id}>
         <label className={`dealer-button dealer-button__${i}`}>D</label>
+      </Maybe>
+      <Maybe cond={!!bet.amount}>
+        <div className={`bet bet__${i}`}>
+          <div className="chips">
+            <img className={`chip chip__1`} src={chip} alt="chip" />
+            <img className={`chip chip__2`} src={chip} alt="chip" />
+            <img className={`chip chip__3`} src={chip} alt="chip" />
+          </div>
+          <label className="bet-amount">{bet.amount}</label>
+        </div>
       </Maybe>
     </li>
   )
