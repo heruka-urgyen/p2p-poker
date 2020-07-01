df721423b0e0fb49a9bfb44c59945be8f1df853c
urgyen <imalyanov@gmail.com>
2020-06-30 21:24:14 -0500

    use lazy maybe to prevent reading players too early

--- src/client/components/Table/Table.js
+++ src/client/components/Table/Table.js
@@ -4,10 +4,6 @@ import Player from './Player'
 import {Maybe} from 'client/util'
 
 function Table({user, table, players}) {
-  const otherPlayers = table.players
-    .filter(id => id !== user.id)
-    .map(id => players[id])
-
   return (
     <div className="table">
       <Maybe cond={table.players.length < 2}>
@@ -19,9 +15,12 @@ function Table({user, table, players}) {
         <Maybe cond={user.type !== 'guest'}>
           <Player key={1} i={1} player={user} />
         </Maybe>
-        {otherPlayers.map((p, i) =>
-          <Player key={i + 2} i={i + 2} player={p} />
-        )}
+        <Maybe cond={players != null}>
+          {() => table.players
+            .filter(id => id !== user.id)
+            .map((id, i) => <Player key={i + 2} i={i + 2} player={players[id]} />
+          )}
+        </Maybe>
       </ul>
     </div>
   )
