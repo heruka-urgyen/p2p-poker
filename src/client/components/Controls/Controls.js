9d6f95a7d15390edc179cb55f0eeec659dea188d
urgyen <imalyanov@gmail.com>
2020-07-11 17:40:30 -0500

    make bet controls draggable

--- src/client/components/Controls/Controls.js
+++ src/client/components/Controls/Controls.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {useDispatch} from 'react-redux'
+import Draggable from 'react-draggable'
 
 import {fold, bet} from 'client/reducers/round'
 
@@ -7,61 +8,64 @@ function Controls({round, player, stack, minBet, isDisabled}) {
   const dispatch = useDispatch()
 
   const [betAmount, updateBet] = useState(minBet)
+
   return (
-    <div className="controls">
-      <div className="controls__adjust-bet">
-        <button
-          className="controls__button"
-          disabled={isDisabled}
-          onClick={_ => updateBet(3 * round.blinds.snd)}
-        >{"3BB"}</button>
+    <Draggable>
+      <div className="controls">
+        <div className="controls__adjust-bet">
+          <button
+            className="controls__button"
+            disabled={isDisabled}
+            onClick={_ => updateBet(3 * round.blinds.snd)}
+          >{"3BB"}</button>
 
-        <button
-          className="controls__button"
-          disabled={isDisabled}
-          onClick={_ => updateBet(stack)}
-        >All in</button>
+          <button
+            className="controls__button"
+            disabled={isDisabled}
+            onClick={_ => updateBet(stack)}
+          >All in</button>
+        </div>
+
+        <div className="controls__bet-size">
+          <input
+            className="bet-size__input"
+            type="number"
+            placeholder="Bet size"
+            value={betAmount || minBet}
+            onChange={e => updateBet(Math.min(e.target.value, stack))}
+            disabled={isDisabled} />
+
+          <input
+            className="bet-size__slider"
+            type="range"
+            min={minBet}
+            max={stack}
+            value={betAmount || minBet}
+            onChange={e => updateBet(e.target.value)}
+            disabled={isDisabled}
+          />
+        </div>
+        <div className="controls__bet-controls">
+          <button
+            className="controls__button"
+            disabled={isDisabled}
+            onClick={_ => dispatch(fold(player))}
+          >Fold</button>
+
+          <button
+            className="controls__button"
+            disabled={isDisabled}
+            onClick={_ => dispatch(bet({player, amount: minBet}))}
+          >{minBet === 0? "Check" : "Call"}</button>
+
+          <button
+            className="controls__button"
+            disabled={isDisabled || minBet > parseInt(betAmount)}
+            onClick={_ => dispatch(bet({player, amount: parseInt(betAmount)}))}
+          >Bet</button>
+        </div>
       </div>
-
-      <div className="controls__bet-size">
-        <input
-          className="bet-size__input"
-          type="number"
-          placeholder="Bet size"
-          value={betAmount || minBet}
-          onChange={e => updateBet(Math.min(e.target.value, stack))}
-          disabled={isDisabled} />
-
-        <input
-          className="bet-size__slider"
-          type="range"
-          min={minBet}
-          max={stack}
-          value={betAmount || minBet}
-          onChange={e => updateBet(e.target.value)}
-          disabled={isDisabled}
-        />
-      </div>
-      <div className="controls__bet-controls">
-        <button
-          className="controls__button"
-          disabled={isDisabled}
-          onClick={_ => dispatch(fold(player))}
-        >Fold</button>
-
-        <button
-          className="controls__button"
-          disabled={isDisabled}
-          onClick={_ => dispatch(bet({player, amount: minBet}))}
-        >{minBet === 0? "Check" : "Call"}</button>
-
-        <button
-          className="controls__button"
-          disabled={isDisabled || minBet > parseInt(betAmount)}
-          onClick={_ => dispatch(bet({player, amount: parseInt(betAmount)}))}
-        >Bet</button>
-      </div>
-    </div>
+    </Draggable>
   )
 }
 
