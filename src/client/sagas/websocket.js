c8516a170cc48cf1243d29191cc4aa07ae7e2965
urgyen <imalyanov@gmail.com>
2020-07-18 20:03:42 -0500

    add initial implementation of p2p channel

--- src/client/sagas/websocket.js
+++ src/client/sagas/websocket.js
@@ -3,7 +3,11 @@ import {eventChannel} from 'redux-saga'
 import {subscribe} from './index.js'
 
 import io from 'socket.io-client'
+import Peer from 'peerjs'
+
 const createWebSocketConnection = () => io('http://localhost:3001')
+
+const P2PSERVER = {host: 'localhost', port: '9000', path: '/poker'}
 
 function createSocketChannel(socket) {
   return eventChannel(emit => {
@@ -17,6 +21,41 @@ function createSocketChannel(socket) {
       socket.off('message', handler)
     }
   })
+}
+
+function createP2PConnectionChannel(peer) {
+  return eventChannel(emit => {
+    peer.on('connection', connection => emit(connection))
+
+    return () => {
+    }
+  })
+}
+
+function createP2PChannel(connection) {
+  return eventChannel(emit => {
+    connection.on('data', data => emit(data))
+
+    return () => {
+    }
+  })
+}
+
+export function* connectP2P(id) {
+  const peer = yield call(() => new Peer(id, P2PSERVER))
+
+  const connectionChannel = yield call(createP2PConnectionChannel, peer)
+  const connection = yield take(connectionChannel)
+  const p2pChannel = yield call(createP2PChannel, connection)
+
+  while (true) {
+    try {
+      const action = yield take(p2pChannel)
+      yield put(action)
+    } catch(err) {
+      console.error('p2p error:', err)
+    }
+  }
 }
 
 export function* connectToWebsocket() {
