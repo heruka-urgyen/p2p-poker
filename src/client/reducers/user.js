9d37aadee6cb9d7af5b725c1a8cee267316fd3de
urgyen <imalyanov@gmail.com>
2020-07-21 17:16:50 -0500

    conditionally disallow updating user on sit_user
    if user is not a guest

--- src/client/reducers/user.js
+++ src/client/reducers/user.js
@@ -2,7 +2,8 @@ import {createReducer} from '@reduxjs/toolkit'
 
 const defaultState = {type: 'guest'}
 const userReducer = createReducer(defaultState, {
-  SIT_USER_SUCCESS: (_, {payload: {user}}) => {
+  SIT_USER_SUCCESS: (s, {payload: {user}}) => {
+    if (s.type !== 'guest') {return s}
     return user
   },
 })
