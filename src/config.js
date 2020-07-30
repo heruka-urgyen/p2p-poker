135a3617518a3680b87cd1d2af51e84e6030c8e7
urgyen <imalyanov@gmail.com>
2020-07-29 22:04:30 -0500

    update server config
    define:
    + dev config
    + prod config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,21 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+const p2pServerConfigDev = {
+  host: 'localhost',
+  port: '9000',
+  path: '/poker',
+  debug: 2,
+}
+
+const p2pServerConfigProd = {
+  host: 'p2p-poker.herokuapp.com',
+  port: '443',
+  path: '/poker',
+  secure: true,
+}
+
+
 module.exports = {
-  p2pServerConfig: {
-    host: 'localhost',
-    port: '9000',
-    path: '/poker',
-    debug: 2,
-  },
+  p2pServerConfig: isProd? p2pServerConfigProd : p2pServerConfigDev
 }
 
